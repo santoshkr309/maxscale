7092120d9e85cb798579a0aff1db797df0ad7c7c
santoshkr309 <68473042+santoshkr309@users.noreply.github.com>
2022-04-05 15:45:36 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,21 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('templates'))
+confd_template = env.get_template('/tmp/maxscale/component_subcomponent_template.cnf')
+cnf_template = env.get_template('/tmp/maxscale/maxscale.cnf')
+values = yaml.load(open('/tmp/maxscale/component_subcomponent_template.cnf'))
+
+for service in values["onboarded_components"]:
+    file=open(service['component']+"_"+service['subcomponent']+".cnf", "w")
+    file.write(confd_template.render(service))
+    file.close()
+
+file = open("/tmp/maxscale/maxscale.cnf","w")
+server_csv = []
+for server in values["onboarded_components"]:
+    server_csv.append("server-"+server['component']+'_'+server['subcomponent'])
+
+servers_csv = ','.join(server_csv)
+file.write(cnf_template.render(servers_csv=servers_csv))
+file.close()
