4e8cc669d268031fc97997a2e6acb6e83e6936a9
santoshkr309 <68473042+santoshkr309@users.noreply.github.com>
2022-04-05 16:38:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,14 +4,14 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
 confd_template = env.get_template('component_subcomponent_template.cnf')
 cnf_template = env.get_template('maxscale.cnf')
-values = yaml.safe_load(open('mx-component.yaml'))
+values = yaml.safe_load(open('templates/mx-component.yaml'))
 
 for service in values["onboarded_components"]:
     file=open(service['component']+"_"+service['subcomponent']+".cnf", "w")
     file.write(confd_template.render(service))
     file.close()
 
-file = open("maxscale.cnf","w")
+file = open("templates/maxscale.cnf","w")
 server_csv = []
 for server in values["onboarded_components"]:
     server_csv.append("server-"+server['component']+'_'+server['subcomponent'])
